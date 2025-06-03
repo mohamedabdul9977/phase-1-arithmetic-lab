ce0602f0089ca512fbb82a7dbd7964e588278717
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-06-03 21:22:38 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+const random = 10;
+(Math.random()* random);
+
+const num3 = 9;
+const num4 = 67;
+
+const mod = num4 % num3;
+
+
+const max = (1,2,3,4,20);
+Math.max(max);
